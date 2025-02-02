b52549b0aa0e6fa764313a40ec176e2eed37a81c
Paco <127772244+pacojf93@users.noreply.github.com>
2025-02-02 17:02:52 +0100

    refactored env variables

--- src/modules/driver.js
+++ src/modules/driver.js
@@ -1,6 +1,6 @@
 const driver = (() => {
-    const VISUAL_CROSSING_URL = process.env.VISUAL_CROSSING_URL
-    const KEY = process.env.KEY
+    KEY = "FXSRC56ZYMBXRYPEZYWZHND42"
+    VISUAL_CROSSING_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
     return (location) =>  fetch(`${VISUAL_CROSSING_URL}/${location}?key=${KEY}&unitGroup=metric`)
 })()
 
